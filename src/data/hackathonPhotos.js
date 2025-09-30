d847a920ddbed6d6b0778354a7a5d984c2e0295d
Ayush Bhardwaj <116327330+Ayush7970@users.noreply.github.com>
2025-09-30 16:25:46 -0500

    adding berkely and Mhacks images

--- src/data/hackathonPhotos.js
+++ src/data/hackathonPhotos.js
@@ -6,6 +6,16 @@ export const hackathonPhotos = [
     location: "Northwestern University, Evanston, IL"
   },
   {
+    image: "/Berkeley1.png",
+    title: "UC Berkeley AI Hackathon 2025",
+    location: "University of California, Berkeley, Berkeley, CA"
+  },
+  {
+    image: "/Mhacks1.png",
+    title: "Mhacks 2025",
+    location: "University of Michigan, Ann Arbor, MI"
+  },
+  {
     image: "/Uchicagohacks.png",
     title: "UChicago Hacks 2025",
     location: "University of Chicago, Chicago, IL"
@@ -24,6 +34,16 @@ export const hackathonPhotos = [
     image: "/headshot2.png",
     title: "SparkHacks 2025",
     location: "University of Illinois Chicago, Chicago, IL"
-  }
+  },
+  {
+    image: "/Berkeley2.png",
+    title: "UC Berkeley AI Hackathon 2025",
+    location: "University of California, Berkeley, Berkeley, CA"
+  },
+  {
+    image: "/Mhacks2.png",
+    title: "Mhacks 2025",
+    location: "University of Michigan, Ann Arbor, MI"
+  },
   // Add more entries as needed...
 ];
